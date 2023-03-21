2f7e7d8be7f3fa531361cae4532a3cfa0b38e49e
Leiciane Menezes <ldsm.ecp19@uea.edu.br>
2023-03-21 20:41:25 -0300

    quinto commit

---
+++ src/main/java/uea/pagamentos_api/repositories/filters/PessoaFilter.java
@@ -0,0 +1,16 @@
+package uea.pagamentos_api.repositories.filters;
+
+import uea.pagamentos_api.models.Endereco;
+
+public class PessoaFilter {
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
